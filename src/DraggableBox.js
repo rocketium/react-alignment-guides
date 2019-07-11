ed60d16d881ae3f10549b1af1511d437554e5c63
Ilango Rajagopal <ilango@rocketium.com>
2019-07-11 10:24:15 +0530

    Add draggable box component

---
+++ src/DraggableBox.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Draggable from 'react-draggable';
+import styles from './styles.scss'
+
+class DraggableBox extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {};
+		this.onDragHandler = this.onDragHandler.bind(this);
+	}
+
+	onDragHandler(e, data) {
+		this.props.onDragHandler(e, data);
+	}
+
+	render() {
+		const { defaultPosition, dimensions, id, isSelected } = this.props;
+		const boxClassNames = isSelected ? `${styles.box} ${styles.selected}` : styles.box;
+		const boxStyles = {
+			width: `${dimensions.width}px`,
+			height: `${dimensions.height}px`
+		};
+
+		return <Draggable defaultPosition={defaultPosition} bounds='parent' onStop={this.onDragHandler}>
+			<div id={id} className={boxClassNames} style={boxStyles} />
+		</Draggable>
+	}
+}
+
+export default DraggableBox;
