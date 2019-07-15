884a5afeab2104ae1b0e61d21a1d7a419b84ef4f
Ilango Rajagopal <ilango@rocketium.com>
2019-07-15 20:04:09 +0530

    Add key map and handlers for moving boxes

--- src/DraggableBox.js
+++ src/DraggableBox.js
@@ -1,16 +1,106 @@
 import React, { Component } from 'react';
 import Draggable from 'react-draggable';
-import styles from './styles.scss'
+import { HotKeys } from "react-hotkeys";
+import { KEY_MAP } from './constants';
+import styles from './styles.scss';
 
 class DraggableBox extends Component {
 	constructor(props) {
 		super(props)
-		this.state = {};
+		this.state = {
+			position: {}
+		};
 		this.onDragHandler = this.onDragHandler.bind(this);
+		this.moveLeftBy1Pixel = this.moveLeftBy1Pixel.bind(this);
+		this.moveRightBy1Pixel = this.moveRightBy1Pixel.bind(this);
+		this.moveUpBy1Pixel = this.moveUpBy1Pixel.bind(this);
+		this.moveDownBy1Pixel = this.moveDownBy1Pixel.bind(this);
+		this.moveLeftBy10Pixels = this.moveLeftBy10Pixels.bind(this);
+		this.moveRightBy10Pixels = this.moveRightBy10Pixels.bind(this);
+		this.moveUpBy10Pixels = this.moveUpBy10Pixels.bind(this);
+		this.moveDownBy10Pixels = this.moveDownBy10Pixels.bind(this);
+	}
+
+	componentDidMount() {
+		this.setState({
+			position: this.props.defaultPosition
+		});
 	}
 
 	onDragHandler(e, data) {
 		this.props.onDragHandler(e, data);
+	}
+
+	moveLeftBy1Pixel(event) {
+		this.setState({
+			position: {
+				...this.state.position,
+				x: this.state.position.x - 1
+			}
+		});
+	}
+
+	moveRightBy1Pixel(event) {
+		this.setState({
+			position: {
+				...this.state.position,
+				x: this.state.position.x + 1
+			}
+		});
+	}
+
+	moveUpBy1Pixel(event) {
+		this.setState({
+			position: {
+				...this.state.position,
+				y: this.state.position.y - 1
+			}
+		});
+	}
+
+	moveDownBy1Pixel(event) {
+		this.setState({
+			position: {
+				...this.state.position,
+				y: this.state.position.y + 1
+			}
+		});
+	}
+
+	moveLeftBy10Pixels(event) {
+		this.setState({
+			position: {
+				...this.state.position,
+				x: this.state.position.x - 10
+			}
+		});
+	}
+
+	moveRightBy10Pixels(event) {
+		this.setState({
+			position: {
+				...this.state.position,
+				x: this.state.position.x + 10
+			}
+		});
+	}
+
+	moveUpBy10Pixels(event) {
+		this.setState({
+			position: {
+				...this.state.position,
+				y: this.state.position.y - 10
+			}
+		});
+	}
+
+	moveDownBy10Pixels(event) {
+		this.setState({
+			position: {
+				...this.state.position,
+				y: this.state.position.y + 10
+			}
+		});
 	}
 
 	render() {
@@ -21,9 +111,22 @@ class DraggableBox extends Component {
 			height: `${dimensions.height}px`
 		};
 
-		return <Draggable defaultPosition={defaultPosition} bounds='parent' onDrag={this.onDragHandler}>
-			<div id={id} className={boxClassNames} style={boxStyles} />
-		</Draggable>
+		const HANDLERS = {
+			'moveLeftBy1Pixel': this.moveLeftBy1Pixel,
+			'moveRightBy1Pixel': this.moveRightBy1Pixel,
+			'moveUpBy1Pixel': this.moveUpBy1Pixel,
+			'moveDownBy1Pixel': this.moveDownBy1Pixel,
+			'moveLeftBy10Pixels': this.moveLeftBy10Pixels,
+			'moveRightBy10Pixels': this.moveRightBy10Pixels,
+			'moveUpBy10Pixels': this.moveUpBy10Pixels,
+			'moveDownBy10Pixels': this.moveDownBy10Pixels
+		};
+
+		return <HotKeys keyMap={KEY_MAP} handlers={HANDLERS}>
+			<Draggable defaultPosition={defaultPosition} position={this.state.position} bounds='parent' onDrag={this.onDragHandler}>
+				<div id={id} className={boxClassNames} style={boxStyles} onClick={this.props.selectBox} />
+			</Draggable>
+		</HotKeys>;
 	}
 }
 
---
+++ src/constants.js
@@ -0,0 +1,12 @@
+// Key map for changing the position and size of draggable boxes
+// Key map follows the Mousetrap syntax: https://craig.is/killing/mice
+export const KEY_MAP = {
+	'moveLeftBy1Pixel': 'left',
+	'moveRightBy1Pixel': 'right',
+	'moveUpBy1Pixel': 'up',
+	'moveDownBy1Pixel': 'down',
+	'moveLeftBy10Pixels': 'shift+left',
+	'moveRightBy10Pixels': 'shift+right',
+	'moveUpBy10Pixels': 'shift+up',
+	'moveDownBy10Pixels': 'shift+down'
+};
