7084d2b315a125896489b9421f62d5a8bad8c798
Ilango Rajagopal <ilango@rocketium.com>
2019-07-23 13:34:33 +0530

    Update rollup config and remove old dist files

--- rollup.config.js
+++ rollup.config.js
@@ -13,11 +13,6 @@ export default {
 	input: 'src/index.js',
 	output: [
 		{
-			file: pkg.main,
-			format: 'cjs',
-			sourcemap: true
-		},
-		{
 			file: pkg.module,
 			format: 'es',
 			sourcemap: true
