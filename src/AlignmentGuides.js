9645261244a6690eca7df7f37ebf569456d70162
Ilango Rajagopal <rajagopalilango@gmail.com>
2019-10-24 08:19:28 +0530

    Remove componentDidUpdate (temporarily)

--- src/AlignmentGuides.js
+++ src/AlignmentGuides.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import equal from 'fast-deep-equal';
 import Box from './Box';
 import { calculateGuidePositions, proximityListener } from './utils/helpers';
 import styles from './styles.scss';
@@ -67,37 +66,37 @@ class AlignmentGuides extends Component {
 		}
 	}
 
-	componentDidUpdate(prevProps) {
-		// Set the dimensions of the bounding box and the draggable boxes
-		// when the component receives new boxes and/or style props.
-		// This is to allow dynamically updating the component by changing the number of boxes,
-		// updating existing boxes by external methods or updating the size of the bounding box
-		if (!equal(prevProps.boxes, this.props.boxes)) {
-			const boundingBox = this.boundingBox.current.getBoundingClientRect().toJSON();
-			const boxes = {};
-			const guides = {};
-
-			// Adding the guides for the bounding box to the guides object
-			guides.boundingBox = {
-				x: calculateGuidePositions(boundingBox, 'x').map(value => value - boundingBox.left),
-				y: calculateGuidePositions(boundingBox, 'y').map(value => value - boundingBox.top)
-			};
-
-			this.props.boxes.forEach((dimensions, index) => {
-				boxes[`box${index}`] = dimensions;
-				guides[`box${index}`] = {
-					x: calculateGuidePositions(dimensions, 'x'),
-					y: calculateGuidePositions(dimensions, 'y')
-				};
-			});
-
-			this.setState({
-				boundingBox,
-				boxes,
-				guides
-			});
-		}
-	}
+	// componentDidUpdate(prevProps) {
+	// 	// Set the dimensions of the bounding box and the draggable boxes
+	// 	// when the component receives new boxes and/or style props.
+	// 	// This is to allow dynamically updating the component by changing the number of boxes,
+	// 	// updating existing boxes by external methods or updating the size of the bounding box
+	// 	if (prevProps.boxes.length !== this.props.boxes.length) {
+	// 		const boundingBox = this.boundingBox.current.getBoundingClientRect().toJSON();
+	// 		const boxes = {};
+	// 		const guides = {};
+	//
+	// 		// Adding the guides for the bounding box to the guides object
+	// 		guides.boundingBox = {
+	// 			x: calculateGuidePositions(boundingBox, 'x').map(value => value - boundingBox.left),
+	// 			y: calculateGuidePositions(boundingBox, 'y').map(value => value - boundingBox.top)
+	// 		};
+	//
+	// 		this.props.boxes.forEach((dimensions, index) => {
+	// 			boxes[`box${index}`] = dimensions;
+	// 			guides[`box${index}`] = {
+	// 				x: calculateGuidePositions(dimensions, 'x'),
+	// 				y: calculateGuidePositions(dimensions, 'y')
+	// 			};
+	// 		});
+	//
+	// 		this.setState({
+	// 			boundingBox,
+	// 			boxes,
+	// 			guides
+	// 		});
+	// 	}
+	// }
 
 	componentWillUnmount() {
 		document.removeEventListener('click', this.unSelectBox);
