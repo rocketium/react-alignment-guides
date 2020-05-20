ab0ad543a1e31ca7ce1deff02e9de5afbe7beff9
Paresh Chouhan <paresh@rocketium.com>
2020-05-20 09:46:47 +0530

    Added selections data

--- src/AlignmentGuides.js
+++ src/AlignmentGuides.js
@@ -258,6 +258,11 @@ class AlignmentGuides extends Component {
 		if (this.state.boxes[data.node.id].metadata) {
 			newData.metadata = this.state.boxes[data.node.id].metadata;
 		}
+		if (data.type && data.type === 'group') {
+			newData.selections = this.state.activeBoxes.map(box => {
+				return Object.assign({}, this.state.boxes[box]);
+			});
+		}
 
 		this.props.onDragStart && this.props.onDragStart(e, newData);
 
@@ -277,10 +282,10 @@ class AlignmentGuides extends Component {
 				newData.metadata = this.state.boxes[this.state.active].metadata;
 			}
 			if (data.type && data.type === 'group') {
-			newData.selections = this.state.activeBoxes.map(box => {
-				return Object.assign({}, this.state.boxes[box]);
-			});
-		}
+				newData.selections = this.state.activeBoxes.map(box => {
+					return Object.assign({}, this.state.boxes[box]);
+				});
+			}
 
 			this.props.onDrag && this.props.onDrag(e, newData);
 		}
